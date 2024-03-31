d7e660846bda4e77e0a914b0cb93ad17fca98fde
孙勇 <sunyong@avic-intl.cn>
2024-03-31 09:23:00 +0800

    增加薪资筛选条件

--- run.py
+++ run.py
@@ -12,5 +12,5 @@ import time
 if __name__ == '__main__':
     browser = driver()
     search_instance = SearchJobPageOperation(browser)
-    search_instance.start(job_name='移动端测试工程师')
+    search_instance.start(job_name=['移动端测试工程师', '软件测试工程师', '自动化测试工程师'], salary='between_10k_to_20k')
     time.sleep(3)
